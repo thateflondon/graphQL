99abcd5ef7d0d1a1cf4250ce9e455c4449ddbab3
Jerry Frederic <jfpl.dev@gmail.com>
2022-02-23 09:41:48 +0100

    bug: expressGraphQL is not define

--- server.js
+++ server.js
@@ -1,5 +1,11 @@
 const express = require("express");
-
+const expressGraphQL = require("express-graphql");
 const app = express();
 
+app.use(
+  "/graphql",
+  expressGraphQL({
+    graphiql: true,
+  })
+);
 app.listen(5000, () => console.log("server running"));
