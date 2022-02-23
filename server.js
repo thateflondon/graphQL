9f7a0bd7a7c1bcf6db8120a63fc9876be927572e
Jerry Frederic <jfpl.dev@gmail.com>
2022-02-23 10:21:44 +0100

    set graphiql and schema

--- server.js
+++ server.js
@@ -1,12 +1,33 @@
 const express = require("express");
-// const expressGraphQL = require("express-graphql");
+// const expressGraphQL = require("express-graphql"); not working
 const { graphqlHTTP } = require("express-graphql");
+
+const { GraphQLSchema, GraphQLObjectType, GraphQLString } = require("graphql");
+
 const app = express();
 
+//create schema
+const schema = new GraphQLSchema({
+  query: new GraphQLObjectType({
+    name: "Hello World",
+    fields: () => ({
+      message: {
+        type: GraphQLString,
+        resolve: () => "Hello World",
+      },
+    }),
+  }),
+});
+
+//create route
 app.use(
   "/graphql",
   graphqlHTTP({
+    //show the interface
     graphiql: true,
+    //render schema
+    schema: schema,
   })
 );
+//set server
 app.listen(5000, () => console.log("server running"));
